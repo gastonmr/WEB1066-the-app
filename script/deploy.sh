9fdbb9a4f9dd87c20c0b96900107a91afc659481
Gaston Morales <gaston.morales@estudiante.ceibal.edu.uy>
2020-07-11 23:07:25 -0500

    Step 10 - New execute for deploy.sh
    works?

--- script/deploy.sh
+++ script/deploy.sh
@@ -3,6 +3,5 @@
 set -e
 
 bash -c "echo \"${DOCKER_PASSWORD}\" | docker login --username \"${DOCKER_USERNAME}\" --password-stdin ;"
-docker pull gastonmr/monolithic-shop
-docker tag gastonmr/monolithic-shop:latest ${DOCKER_USERNAME}/monolithic:latest
+docker tag zutherb/monolithic-shop:latest ${DOCKER_USERNAME}/monolithic:latest
 docker push ${DOCKER_USERNAME}/monolithic:latest
